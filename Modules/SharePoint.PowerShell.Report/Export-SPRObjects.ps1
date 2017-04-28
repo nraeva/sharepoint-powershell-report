0a7e13454b99a05bb3253de68c4ee08347ee28ed
evgeni <evgeni.petrov@gmail.com>
2017-04-28 16:16:46 -0700

    Uncommented all exports.

--- Modules/SharePoint.PowerShell.Report/Export-SPRObjects.ps1
+++ Modules/SharePoint.PowerShell.Report/Export-SPRObjects.ps1
@@ -11,20 +11,20 @@
 	
   Add-PSSnapin -Name Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue
   
-  #Export-SPRFarmConfiguration -Path $Path
-  #Export-SPRDatabaseConfiguration -Path $Path
-  #Export-SPRServerConfiguration -Path $Path
+  Export-SPRFarmConfiguration -Path $Path
+  Export-SPRDatabaseConfiguration -Path $Path
+  Export-SPRServerConfiguration -Path $Path
   Export-SPRWebApplicationConfiguration -Path $Path
-  #Export-SPRSiteConfiguration -Path $Path
-  #Export-SPRServiceInstanceConfiguration -Path $Path
-  #Export-SPREnterpriseSearchServiceApplicationConfiguration -Path $Path
-  #Export-SPREnterpriseSearchTopologyConfiguration -Path $Path
-  #Export-SPREnterpriseSearchComponent -Path $Path
-  #Export-SPRFeature -Path $Path
-  #Export-SPRManagedPath -Path $Path
-  #Export-SPRManagedAccount -Path $Path
-  #Export-SPRAlternateUrl -Path $Path
-  #Export-SPRAuthenticationProvider -Path $Path
+  Export-SPRSiteConfiguration -Path $Path
+  Export-SPRServiceInstanceConfiguration -Path $Path
+  Export-SPREnterpriseSearchServiceApplicationConfiguration -Path $Path
+  Export-SPREnterpriseSearchTopologyConfiguration -Path $Path
+  Export-SPREnterpriseSearchComponent -Path $Path
+  Export-SPRFeature -Path $Path
+  Export-SPRManagedPath -Path $Path
+  Export-SPRManagedAccount -Path $Path
+  Export-SPRAlternateUrl -Path $Path
+  Export-SPRAuthenticationProvider -Path $Path
     
   Write-Host -Object 'Waiting on exports to complete...' -NoNewline
   $null = Get-Job |
